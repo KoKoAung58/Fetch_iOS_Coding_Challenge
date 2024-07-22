359b900049ac55cbe5638d5e27401f866173a7d4
Ko Ko Aung <kaung@oswego.edu>
2024-07-21 22:30:44 -0700

    Initial Commit

---
+++ Fetch_iOS_Challenge/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Fetch_iOS_Challenge
+//
+//  Created by Ob yda on 7/21/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Fetch_iOS_Challenge/Fetch_iOS_ChallengeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fetch_iOS_ChallengeApp.swift
+//  Fetch_iOS_Challenge
+//
+//  Created by Ob yda on 7/21/24.
+//
+
+import SwiftUI
+
+@main
+struct Fetch_iOS_ChallengeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
